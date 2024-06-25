ff8f1aacb662d22d6e85c58055133867e96c67c5
fname0 <kmailfw@mail.ru>
2024-06-25 12:59:07 +0300

    x

--- api/index.py
+++ api/index.py
@@ -1,7 +1,21 @@
 from flask import Flask
+import requests
+from requests.auth import HTTPDigestAuth
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    params = {
+        'action': 'openDoor',
+        'channel': '1',
+        'UserID': '101',
+        'Type': 'Remote',
+    }
+
+    response = requests.get(
+        'http://95.174.126.144/cgi-bin/accessControl.cgi',
+        params=params,
+        auth=HTTPDigestAuth('admin', 'PG19mega'),
+    )
+    return response.content
